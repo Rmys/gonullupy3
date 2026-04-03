7902f5642d5fe6f0e5021a8433cbcec59f0eeeaf
Rmys <muscnsl@gmail.com>
2026-04-03 22:30:39 +0300

    docker 29

--- Gdocker.py
+++ Gdocker.py
@@ -30,7 +30,7 @@ class Docker:
         # containerımızı parametreleri ile çalıştıracağımız fonksiyonumuz.
         if not self.my_client:
             # my_client'de çalışan docker process'ini yakalıyorum.
-            self.my_client = APIClient(base_url='unix://var/run/docker.sock', version='1.35')
+            self.my_client = APIClient(base_url='unix://var/run/docker.sock', version='1.44')
 
         # container'ımızın host configlerini yapalım.
         self.host_config = self.my_client.create_host_config(mem_limit='%sM' % self.memory_limit, binds=self.binds, security_opt=['seccomp:unconfined'])
